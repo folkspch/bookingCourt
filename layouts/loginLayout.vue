cb62e913a108734baed83ab70793f4016baaa977
folk <folk_spch@hotmail.com>
2022-05-15 14:33:36 +0700

    login

---
+++ layouts/loginLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app back>
+    <nuxt />
+  </v-app>
+</template>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Prompt:wght@100;300;500&display=swap");
+* {
+  font-family: "Prompt", sans-serif;
+}
+.v-application{
+  background:#a1cae2;
+}
+</style>
